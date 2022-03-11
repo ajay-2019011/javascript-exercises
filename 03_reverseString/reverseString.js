affd4e3c99f4261be24fe73f51607caac357a876
ajay-2019011 <ajaysheoran2019011@gmail.com>
2022-03-11 17:18:54 +0530

    Excercise reverseString done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  if (!string)
+    return string;
+  let splitStrings = string.split(" ");
+  //If there is only a single word in string, then after this splitting
+  //Will the single word be represented as splitStrings[0]?
+  for (let i = 0; i < splitStrings.length; i++) {
+    let splitString = splitStrings[i].split("");
+    let reverseSplitString = splitString.reverse();
+    let finalReverseString = reverseSplitString.join("");
+    splitStrings[i] = finalReverseString;
+  }
+  let reverseStrings = splitStrings.reverse();
+  let finalReverseStrings = reverseStrings.join(" ");
+  return finalReverseStrings;
 };
 
 // Do not edit below this line
