718609167d3e2f6706b9b79234e7c59b73c28b73
ajay-2019011 <ajaysheoran2019011@gmail.com>
2022-03-11 20:36:26 +0530

    Excercise repeatString done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (!string)
+    return '';
+  if (num < 0)
+    return 'ERROR';
+  if (num == 0)
+    return '';
+  let resultString=string;
+  for (let i = 1; i < num; i++){
+    resultString+=string;
+  }
+  //let resultString = arrayOfStrings.toString();
+  return resultString;
 };
 
 // Do not edit below this line
