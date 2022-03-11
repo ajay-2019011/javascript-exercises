4fde77aae573fb1b8f933cddbb68972fe72905b2
ajay-2019011 <ajaysheoran2019011@gmail.com>
2022-03-11 23:55:14 +0530

    Excercise sumAll done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+  let small, big;
+  if (typeof (first) != 'number' || typeof (second) != 'number')
+    return 'ERROR';
+  
+  if (first < 0 || second < 0)
+    return 'ERROR';
+  
+  if (first < second) {
+    small = first;
+    big = second;
+  }
+  if(first > second) {
+    small = second;
+    big = first;
+  }
+  let i, sum=0;
+  for (i = small; i <= big; i++){
+    sum = sum + i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
