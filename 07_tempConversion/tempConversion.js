1b2626d705b053d8443fe64e4ca5806e465432d7
ajay-2019011 <ajaysheoran2019011@gmail.com>
2022-03-12 16:52:44 +0530

    Excercise tempConversion done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahreinheit) {
+  let celsius;
+  celsius = (5 / 9) * (fahreinheit - 32);
+  return parseFloat(celsius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let fahreinheit;
+  fahreinheit = (9 / 5) * celsius + 32;
+  return parseFloat(fahreinheit.toFixed(1));
 };
 
 // Do not edit below this line
