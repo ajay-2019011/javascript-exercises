a3314e7a9615cc72e05c6f5597e6c58639bc0c4a
ajay-2019011 <ajaysheoran2019011@gmail.com>
2022-03-13 16:55:40 +0530

    Excercise palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (inputS) {
+  let outputS = '';
+  let i = 0, len = inputS.length;
+  let inputLower = inputS.toLowerCase();
+  for (i = 0; i < len; i++){
+    if (inputLower[i] >= 'a' && inputLower[i] <= 'z') {
+      outputS = outputS + inputLower[i];
+    }
+  }
+  for (i = 0, j = outputS.length-1; i < outputS.length,j>=0; i++,j--){
+    if (outputS[j] != outputS[i])
+      return false;
+  }
+  return true;
 };
 
 // Do not edit below this line
