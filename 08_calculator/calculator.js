095523f57a8d0f7c43aa66ca7278f29b65d76f54
ajay-2019011 <ajaysheoran2019011@gmail.com>
2022-03-13 21:41:55 +0530

    Excercise calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let len = arr.length;
+  let i, sum = 0;
+  if (len == 0)
+    return 0;
+  for (i = 0; i < len; i++){
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let len = arr.length;
+  let i, product = 1;
+  for (i = 0; i < len; i++){
+    product *= arr[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let i = 0,num=1;
+  for (i = 0; i < b; i++){
+    num = num * a;
+  }
+  return num;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let i = 1, factorial = 1;
+  if (n == 0)
+    return 1;
+  for (i = 1; i <= n; i++){
+    factorial = factorial * i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
