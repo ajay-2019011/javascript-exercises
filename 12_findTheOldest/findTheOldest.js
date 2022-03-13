ef2de2785be3ca91b87ff7c2233c77594523294c
ajay-2019011 <ajaysheoran2019011@gmail.com>
2022-03-13 19:21:07 +0530

    Excercise findTheOldest done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let age, i,maxAge=0,oldest, today, currentYear;
+  for (i = 0; i < people.length; i++){
+    if (people[i].yearOfDeath == null) {
+      today = new Date();
+      currentYear = today.getFullYear();
+      age = currentYear - people[i]['yearOfBirth'];
+    }
+    else  
+      age = people[i]['yearOfDeath'] - people[i]['yearOfBirth'];
+    if (maxAge < age) {
+      maxAge = age;
+      oldest = i;
+    }
+  }
+  return people[oldest];
 };
 
 // Do not edit below this line
