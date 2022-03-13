cc7e396ac257f84204595b8f66882580d94d651c
ajay-2019011 <ajaysheoran2019011@gmail.com>
2022-03-13 17:17:22 +0530

    Excercise fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (m) {
+  let n = parseInt(m);
+  let num1 = 1, num2 = 1, i, nth;
+  if (n < 0)
+    return 'OOPS';
+  if (n == 1 || n == 2)
+    return 1;
+  for (i = 3; i <= n; i++){
+    num1 = num1 + num2;
+    num2 = num1-num2;
+  }
+  nth = num1;
+  return nth;
 };
 
 // Do not edit below this line
