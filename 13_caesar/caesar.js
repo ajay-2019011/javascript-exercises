b7a7412c23e601a2b3f9e9869adbe0218cf4264a
ajay-2019011 <ajaysheoran2019011@gmail.com>
2022-03-14 14:01:48 +0530

    Excercise caesar cipher done

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,34 @@
-const caesar = function() {
-
+const caesar = function(string, shift) {
+  let result = '';
+  let i,ascii,gap;
+  for (i = 0; i < string.length; i++){
+    ascii = string.charCodeAt(i);
+    if (ascii >= 65 && ascii <= 90) {
+      ascii = ascii + shift;
+      while (ascii > 90) {
+        gap = ascii - 91;
+        ascii = 65 + gap;
+      }
+      while (ascii < 65) {
+        ascii = ascii + 26 ;
+      }
+    }
+    else if (ascii >= 97 && ascii <= 122) {
+      ascii = ascii + shift;
+      while (ascii > 122) {
+        gap = ascii - 123;
+        ascii = 97 + gap;
+      }
+      while (ascii < 97) {
+        ascii = ascii + 26;
+      }
+    }
+    else
+      ascii = ascii + 0;
+    
+    result = result + String.fromCharCode(ascii);
+  }
+  return result;
 };
 
 // Do not edit below this line
