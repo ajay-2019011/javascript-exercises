94a61ca786947c719051a98e434598d1d4bd6d8e
ajay-2019011 <ajaysheoran2019011@gmail.com>
2022-03-15 16:48:54 +0530

    Excercise removeFromArray using rest parameter and for loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+//rest parameters for variadic functions in Javascript
+//Variadic func- which accepts a variable number of arguments
+const removeFromArray = function (arr, ...theArgs) {
+  let i, j;
+  for (i = 0; i < theArgs.length; i++){
+    for (j = 0; j < arr.length; j++){
+      if (arr[j] === theArgs[i]) {
+        arr.splice(j, 1);
+        j--;
+      }
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
